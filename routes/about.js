adeefcc2b7b011dfe69907d6798931c24e289fc2
vedhapriyaa12 <s.vedhapriyaa@gmail.com>
2022-03-04 09:49:06 +0530

    about pages

--- routes/about.js
+++ routes/about.js
@@ -6,4 +6,14 @@ router.get('/overview', function(req, res) {
     res.render('./about/overview.pug');
 });
 
+// /organisation structure
+router.get('/organisation-structure', function(req, res) {
+    res.render('./about/organisation-structure.pug');
+});
+
+// /messages
+router.get('/messages', function(req, res) {
+    res.render('./about/messages.pug');
+});
+
 module.exports = router;
